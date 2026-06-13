a378bb65c2482d90f764dcc55012f66e1c58f209
IamRPDev <IamRPDev@users.noreply.github.com>
2026-06-13 04:25:32 -0400

    chore: remove otaku-api and otaku-ui assets from container-manager

--- containers/IamRPDev_otaku-api.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name IamRPDev_otaku-api ghcr.io/rpdevs-vault/iamrpdev_otaku-api:latest
--- containers/IamRPDev_otaku-ui.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name IamRPDev_otaku-ui ghcr.io/rpdevs-vault/iamrpdev_otaku-ui:latest
